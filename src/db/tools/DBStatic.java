623b7dcd8f83c5e4c402125b4cd5534b9d30dd50
Wail <wkhecha@gmail.com>
2015-04-05 21:58:36 +0200

    Update DBStatic.java

--- src/db/tools/DBStatic.java
+++ src/db/tools/DBStatic.java
@@ -6,14 +6,14 @@ package db.tools;
 
 public class DBStatic {
 	//mysql
-	public static String mysql_host ="132.227.201.129:33306";
-	public static String mysql_username = "khecha_djamah";
-	public static String mysql_password = "khecha_djamah";
-	public static String mysql_db = "khecha_djamah";
+	public static String mysql_host ="...";
+	public static String mysql_username = "...";
+	public static String mysql_password = "...";
+	public static String mysql_db = "...";
 	public static boolean mysql_pooling =false;
 
 	
 	//mongodb
-	public static String mongoDb_host = "132.227.201.129";
+	public static String mongoDb_host = "...";
 	public static int mongoDb_port = 27130;
 }
